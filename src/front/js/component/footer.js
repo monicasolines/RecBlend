4f8a694378dd41b2d4cdcb1caa581fc37d0728d2
Maikel Cardenas <163206166+maikelcaardenas@users.noreply.github.com>
2024-10-09 16:26:48 +0000

    cambio de footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
+import img from "../../img/peelcompany-removebg-preview.png"
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footer mt-auto py-3 text-center bg-warning">
+		<img className= "h-25 w-25"src= {img} alt = "peel-img"/>
+		<p>© Copyright 2024, peel. </p>
 	</footer>
 );
