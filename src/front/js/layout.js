dafc7534d31756a0716425cef7015156cc5fa039
santgeek <santiago.mordz@gmail.com>
2024-11-03 21:40:36 -0500

    hotfix: instalación de dependencias para corregir error al cargar la página de inicio

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,8 +4,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 import NavBar from "./component/Navbar";
 import { Footer } from "./component/footer";
@@ -28,8 +26,6 @@ const Layout = () => {
                         <Route element={<Home />} path="/home" />
                         <Route element={<Home />} path="/" />
                         <Route element={<DashboardAdmin />} path="/dashboardAdmin" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
                         <Route element={<RegistrationForm />} path="/register" />
                         <Route element={<LoginForm />} path="/login" />
                         <Route element={<h1>Not found!</h1>} />
