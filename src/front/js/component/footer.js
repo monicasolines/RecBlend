6e02fa637e4042856e58701d612153e820d9c4bc
Redando Dieujuste <101023464+9308RD00@users.noreply.github.com>
2024-11-26 16:32:21 +0000

    footer 1

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -6,9 +6,8 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<div>About us</div>
+		<div>Contact us</div>
+		<div>Powered by &copy; Redando</div>
 	</footer>
 );
