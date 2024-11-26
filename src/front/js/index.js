8c023b0bf425d0fd0a13feeb0c25ddf1ac559d4d
George Mihov <53583283+gmihov001@users.noreply.github.com>
2024-11-26 10:47:53 -0500

    Undid test change in index.js

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
-import { Footer } from "./component/footer";
 
 //import your own components
 import Layout from "./layout";
