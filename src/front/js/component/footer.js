524e6fc29a6299060c50191f58d5a2d003a27ede
Redando Dieujuste <101023464+9308RD00@users.noreply.github.com>
2024-11-26 18:47:48 +0000

    footer3

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Sample } from "./sample";
 
 
 
-
-
-export const Footer = () => (
+export const Footer = () => {
 
 
 
-	
+   return(
 	<footer className="footer mt-auto py-3 text-center">
-		<div>About us</div>
-		<div>Contact us</div>
-		<div>Powered by &copy; Redando</div>
+		<Link className="link" to="/">
+         <div style={{color:"white", textDecoration:"none"}}>About us</div>
+		</Link>
+		<Link className="link" to="/">
+		<div style={{color:"white", textDecoration:"none"}}>Contact us</div>
+		</Link>
+		<div>CryptoScope &copy; 2024</div>
+		<div>Powered by CoinGecko</div>
 	</footer>
-);
+   )
+};
