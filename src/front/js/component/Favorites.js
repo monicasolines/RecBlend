b2e48e785af6807a236c674cb8a8141e9aef4c7f
Daniel Alvaro <153893558+danielta18@users.noreply.github.com>
2024-11-27 20:04:06 +0000

    adding fav cards

--- src/front/js/component/Favorites.js
+++ src/front/js/component/Favorites.js
@@ -2,11 +2,28 @@ import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
+import { SparklineChart } from "../pages/sparklineChart";
 
 export const Favorites = () => {
     const { store, actions } = useContext(Context);
 
-    return( 
-        <div>favorites</div>
+    return (
+        <div>
+            {store.favorites.map((favorite, index) => {
+                return (
+                    <div className="favCard card" style={{ width: "20vw"}}>
+                        <div className="favCardTop card-img-top">
+                            <SparklineChart data={favorite.sparkline_in_7d.price} />
+                        </div>
+                        <div className="card-body">
+                            <h5 className="card-title">{favorite.name}</h5>
+                            <p className="card-text">{favorite.symbol}</p>
+                            <p className="card-text">${favorite.current_price}</p>
+                            <a href="#" className="btn btn-primary">More Information</a>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
