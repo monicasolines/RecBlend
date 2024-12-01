7e520ecb15d9468979eae35065c9ec27963b8ee4
Esteban Brea <83199483+estebanbrea@users.noreply.github.com>
2024-12-01 16:18:36 +0000

    [segundo checkpoint] -para moverme a la rama development

--- src/api/routes.py
+++ src/api/routes.py
@@ -24,6 +24,24 @@ def get_pending_users():
     pending_users = User.query.filter_by(status='en_revision').all()
     return jsonify([user.serialize() for user in pending_users]), 200
 
+@app.route('/admin/approve-user/<int:user_id>', methods=['PATCH'])
+@jwt_required()
+def approve_user(user_id):
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(id=current_user).first()
+    
+    if user.role != 'admin':
+        return jsonify({"msg": "Unauthorized"}), 403
+
+    user_to_approve = User.query.get(user_id)
+    if not user_to_approve:
+        return jsonify({"msg": "User not found"}), 404
+
+    # Cambia únicamente el estado del usuario
+    user_to_approve.status = "activo"
+    db.session.commit()
+    return jsonify({"msg": f"User {user_to_approve.username} approved"}), 200
+
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
